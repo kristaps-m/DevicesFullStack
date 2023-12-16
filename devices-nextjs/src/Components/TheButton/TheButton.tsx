743adbfb20570340b34ca01ef8e711a4ca47bfd8
Kristaps-M <kristapsmitins@gmail.com>
2023-12-16 20:21:37 +0200

    remove comments

--- devices-nextjs/src/Components/TheButton/TheButton.tsx
+++ devices-nextjs/src/Components/TheButton/TheButton.tsx
@@ -20,17 +20,8 @@ export const TheButton = ({
   onClick,
 }: Props): JSX.Element => {
   return (
-    <button
-      className={`${buttonClassName}`}
-      // className={`inline-flex items-center justify-center gap-[10px] px-[16px] py-[8px] relative bg-x01-theme-colors01-primary-colorp-300 rounded-[6px] all-[unset] box-border ${className} hover:bg-x01-theme-colors01-primary-colorp-300`}
-      onClick={onClick}
-    >
-      <div
-        className={`${divClassName}`}
-        // className={`relative w-fit [font-family:'Inter',Helvetica] font-medium text-x01-theme-colors02-neutral-colorn-100 text-[14px] text-center tracking-[-0.20px] leading-[20px] whitespace-nowrap ${divClassName}`}
-      >
-        {text}
-      </div>
+    <button className={`${buttonClassName}`} onClick={onClick}>
+      <div className={`${divClassName}`}>{text}</div>
     </button>
   );
 };
